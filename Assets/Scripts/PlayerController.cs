37ce1f3ee2872f483236ef9547fdd1814a8904fc
renfire <renfire@gmail.com>
2016-04-17 02:11:04 +0200

    Escenari i moviment bàsic

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movement = new Vector2(moveHorizontal, 0.0f);
+
+        rb.velocity = (movement);            
+    }
+}
