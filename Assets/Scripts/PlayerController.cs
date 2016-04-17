526358f3942c370f52522050e7ea745c915a3433
renfire <renfire@gmail.com>
2016-04-17 23:16:31 +0200

    intentant animar....

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,14 @@ public class PlayerController : MonoBehaviour {
     private Rigidbody2D rb;
     bool facingRight = true;
     public Camera _camera;
+    private Animator _animator;
+
+    private int _charactersInRange = 0;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -18,19 +22,35 @@ public class PlayerController : MonoBehaviour {
 
         if ((moveHorizontal > 0 && !facingRight) || (moveHorizontal < 0 && facingRight)) Flip();
 
+        if (moveHorizontal == 0) _animator.SetTrigger("startWaiting");
+        else _animator.SetTrigger("startWalking");
+
         Vector2 movement = new Vector2(moveHorizontal, 0.0f);
 
-        rb.velocity = (movement);            
+        rb.velocity = (movement);
+
+        Vector3 characterPosition = transform.position;
+        Vector3 cameraPositon = new Vector3(characterPosition.x, _camera.transform.position.y,  _camera.transform.position.z);
+
+        _camera.transform.position = cameraPositon;
     }
 
     void OnTriggerEnter2D()
     {
-       _camera.GetComponent<AutoZoom>().zoomIn = true;
+        _charactersInRange++;
+        if (_charactersInRange > 0)
+        {
+            _camera.GetComponent<AutoZoom>().zoomIn = true;
+        }           
     }
 
     void OnTriggerExit2D()
     {
-        _camera.GetComponent<AutoZoom>().zoomIn = false;
+        _charactersInRange--;
+        if (_charactersInRange <= 0)
+        {
+            _camera.GetComponent<AutoZoom>().zoomIn = false;
+        }
     }
 
     void Flip()
