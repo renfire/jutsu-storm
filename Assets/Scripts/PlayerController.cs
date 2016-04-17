55cb105f42252e2ceedd0d2aabc4a1866cf21db4
renfire <renfire@gmail.com>
2016-04-17 02:53:33 +0200

    Wood Bridge + Normal bumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     private Rigidbody2D rb;
+    bool facingRight = true;
 
     void Awake()
     {
@@ -14,8 +15,18 @@ public class PlayerController : MonoBehaviour {
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
 
+        if ((moveHorizontal > 0 && !facingRight) || (moveHorizontal < 0 && facingRight)) Flip();
+
         Vector2 movement = new Vector2(moveHorizontal, 0.0f);
 
         rb.velocity = (movement);            
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
