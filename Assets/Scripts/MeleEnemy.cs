ac907a14aabe6607f4b8cb4a172a47b5b58c78bb
Katots <johan.starcraft@gmail.com>
2016-04-17 10:25:12 +0200

    Shuriken i objectiu v0.1

---
+++ Assets/Scripts/MeleEnemy.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleEnemy : MonoBehaviour {
+
+	void Awake () {
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+	
+}
---
+++ Assets/Scripts/ShurikenController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShurikenController : MonoBehaviour {
+
+    public GameObject shuriken;
+    private int ammo = 2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject clone = Instantiate(shuriken, transform.position, transform.rotation) as GameObject;
+            Rigidbody2D rb = clone.GetComponent<Rigidbody2D>();
+
+            Vector2 movement = new Vector2(1f, 0.0f);
+
+            rb.velocity = (movement);
+            rb.angularVelocity   = 720f;
+            Destroy(clone, 6);
+        }
+	}
+}
